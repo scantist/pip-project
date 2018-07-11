dc2122e70d6671b4557af7ea22af110893c8b6e6
scantist-admin <31680701+scantist-admin@users.noreply.github.com>
2018-07-11 19:28:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name=package_name,
     version=package_version,
     author="some author",
-    install_requires=["requests", "numpy", "Django==1.9.6"],
+    install_requires=["requests", "numpy", "Django==1.9.6", "sosreport", "Zope2==2.12.0"],
     entry_points={
         'console_scripts': [
             'pip-project=Main:main'
